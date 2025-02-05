4153a9cd2e9d53f114ac59b677a9597a52ec3d1d
Var <skipjackyu@gmail.com>
2025-02-06 01:22:37 +0800

    WIP: bot command

--- bot/packages/telegram/index.js
+++ bot/packages/telegram/index.js
@@ -2,7 +2,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const request = require('./utils/request.js')
 
 // Replace 'YOUR_BOT_TOKEN' with the token from BotFather
-const token = process.env.TELEGRAM_BOT_TOKEN
+const token = process?.env?.TELEGRAM_BOT_TOKEN || '7926431835:AAEjbScqFyl9W6Qu907xOfoGkZ8EKFpvJgM'
 const bot = new TelegramBot(token, { polling: true });
 
 // Listen for any message
@@ -12,10 +12,21 @@ bot.on('message', async (msg) => {
     const messageText = msg.text;
     const username = msg.from.username || 'Anonymous';
     const userId = msg.from.id;
+    const messageType = msg.entities?.[0]?.type || 'text';
     try {
+        if (messageType === 'bot_command') {
+            const [command, ...args] = messageText.slice(1).split(' ');
+            if (command === 'bind') {
+                const bindId = args[0];
+                console.log('bindId====', bindId)
+                return
+            }
+        }
+
         request({
             url: '/api/add_message',
             data: {
+
                 "group_id": chatId,
                 "user_id": userId,
                 "username": username,
